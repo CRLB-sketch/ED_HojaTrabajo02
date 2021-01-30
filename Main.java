5bf1c6560d9191385f4902bb8d700ccfaf010738
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2021-01-30 13:49:23 -0800

    Creando Programa

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * <h1> Hoja de Trabajo 02 </h1>
+ * <h2> Main (Clase principal) </h2>
+ * 
+ * ADT Calculadora Postfix
+ * 
+ * Esta clase ejecutará el programa completo
+ * 
+ * <p> Algoritmos Estructuras de datos - Universidad del Valle de Guatemala </p>
+ * 
+ * Creado por:
+ * @author Cristian Fernando Laynez Bachez
+ * @version 1.0
+ * @since 2021-Enero-30
+ **/    
+
+ class Main{
+     public static void main(String[] args) {
+         System.out.println("Hola Hoja de Trabajo 02");
+     }
+ }
